0603e539b3452bbaff393136190d80197311678f
vipin_chaudhary-dl <vipin.chaudhary@devslane.com>
2024-12-23 17:30:20 +0530

    API to fetch learners latest repsonses fofr the question set

--- src/services/learnerProficiencyData.ts
+++ src/services/learnerProficiencyData.ts
@@ -69,3 +69,11 @@ export const updateLearnerProficiencyQuestionSetLevelData = async (transaction: 
   });
   return { updatedLearnerData };
 };
+
+export const getAllQuestionLevelDataByLearnerIdAndQuestionSetId = async (learnerId: string, questionSetId: string): Promise<LearnerProficiencyQuestionSetLevelData[]> => {
+  return LearnerProficiencyQuestionLevelData.findAll({
+    where: { learner_id: learnerId, question_set_id: questionSetId },
+    attributes: { exclude: ['id'] },
+    raw: true,
+  });
+};
